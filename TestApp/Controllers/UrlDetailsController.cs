c7f330ad4ec427c532ea2ef49a06b89f65832f4e
DESKTOP-UAK31HS\Kristina <christina_antoniuk@outlook.com>
2018-09-17 19:54:42 +0300

    Initial commit.

---
+++ TestApp/Controllers/UrlDetailsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class UrlDetailsController : Controller
+    {
+        // GET: UrlDetails
+        public async Task<ActionResult> Index(URL model)
+        {
+            string[] urls = model.URLsSet.Split('\n');
+            model.URLs = new string[urls.Length];
+            for (int i = 0; i < urls.Length; i++)
+            {
+                model.URLs[i] = urls[i].Trim('\r', '\n');
+            }
+            model = await GetInfo(model);
+            return View("../Home/UrlDetails", model);
+        }
+
+        static async Task<URL> GetInfo(URL model)
+        {
+            HttpClient client = new HttpClient();
+            var tasksString = new List<Task<string>>();
+            var tasksCodes = new List<Task<HttpResponseMessage>>();
+            foreach (string url in model.URLs) { 
+
+                tasksString.Add(client.GetStringAsync(url));
+                tasksCodes.Add(client.GetAsync(url));
+
+            }   
+            try
+            {
+                await Task.WhenAll(tasksString.ToArray());
+                await Task.WhenAll(tasksCodes.ToArray());
+            }
+            catch { }
+            model.Titles = new string[model.URLs.Length];
+            model.ResStatusCodes = new string[model.URLs.Length];
+
+            for (int i = 0; i < model.URLs.Length; i++)
+            {
+                if (!tasksString[i].IsFaulted)
+                    {
+                        model.Titles[i] = Regex.Match(tasksString[i].Result, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+                        model.ResStatusCodes[i] = tasksCodes[i].Result.StatusCode.ToString();
+                    }
+                else
+                    {
+                        model.Titles[i] = "Faulted";
+                        model.ResStatusCodes[i] = "Faulted";
+                    }
+            }
+            return model;
+        }
+    }
+}
---
+++ TestApp/Models/URL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.Models
+{
+    public class URL
+    {
+        public string URLsSet { get; set; }
+        public string[] URLs { get; set; }
+        public string[] Titles { get; set; }
+        public string[] ResStatusCodes { get; set; }
+    }
+}
